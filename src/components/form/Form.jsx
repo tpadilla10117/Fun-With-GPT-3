a8b368922482c607cd5f472c04edf762102e45bf
Trinidad Padilla <tpadilla101@live.com>
2022-05-10 11:54:56 -0700

    api key returning undefined

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import axios from 'axios';
+import { CtaButton } from '../utils';
+
+require('dotenv').config();
+
+    const apiKey = process.env.OPEN_AI_KEY;
+
+    const BASE_URL = 'https://api.openai.com/v1/engines/text-curie-001/completions';
+    const TEST_BASE_URL = `${apiKey}https://api.openai.com/v1/engines/text-curie-001/`;
 
 function Form( { content } ) {
 
+    const submitForm = () => {
+        /* event.preventDefault(); */
+        console.log('clicked the form button!')
+    /*TODO: Test a basic get request */
+        getRequestTest();
+
+    };
+
+    const getRequestTest = async () => {
+        try {
+            const response = axios.get(TEST_BASE_URL)
+            console.log('Response from the getRequestTest: ', response);
+        } catch(error) {
+            console.error(error);
+        }
+    };
 
     
   return (
@@ -16,13 +40,17 @@ function Form( { content } ) {
 
       <div className='form-wrapper'>
         <form className='form'>
-          <div className='form-input-wrapper'>
-            <label />
-            <textarea rows='5' placeholder='Message' name='message' required/>
-          </div>
+            <div className='form-input-wrapper'>
+                <label />
+                <textarea rows='5' placeholder='Message' name='message' required/>
+            </div>
 
     {/* Where the 'Submit' button is rendered: */}
-        {content}
+            <CtaButton 
+                classnames={'form-submit-btn'}
+                text={'Submit'}
+                clickHandler={submitForm }
+            />
 
         </form>
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,37 +3,15 @@ import './Home.scss';
 import {
     ApiResponses,
     Form,
-    CtaButton
 } from '../../components/utils.js';
-
-require('dotenv').config();
-
-const apiKey = process.env.OPEN_AI_KEY;
-
-const BASE_URL = 'https://api.openai.com/v1/engines/text-curie-001/completions';
-
 
 const HomePage = () => {
 
-    const submitForm = () => {
-        /* event.preventDefault(); */
-        console.log('clicked the form button!')
-    };
-
-    
     return (
         <main id="homepage-parent-container">
             
         {/* TODO: Form Component */}
-            <Form 
-                content={
-                <CtaButton 
-                    classnames={'form-submit-btn'}
-                    text={'Submit'}
-                    clickHandler={submitForm }
-                />
-                }
-            />
+            <Form />
 
             <ApiResponses />
         
