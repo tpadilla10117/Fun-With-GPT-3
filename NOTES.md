7942bb269b12224c5f329ea08d92ed7694abab69
Trinidad Padilla <tpadilla101@live.com>
2022-05-10 10:07:27 -0700

    first comit

--- NOTES.md
+++ NOTES.md
@@ -1,11 +1,10 @@
 <!-- Sample web service requests: -->
-https://api.nasa.gov/planetary/apod?api_key=wd5U8KXHEwH3VFh4LFzXqeqMIW4vUjMqn7EtRgSU
+https://beta.openai.com/overview
 
 <!-- Objectives: -->
-    1) Fetch data from one of NASA’s APIs and display the resulting images [x]
-    2) Display descriptive data for each image (for example: title, date, description, etc.) [x]
-    3) Like an image [x]
-    4) Unlike an image [x]
+    1) Fetch data from OpenAI API []
+    2) Display a form that takes in a prompt as a submission []
+   
 
 
 <!-- Structure: -->
@@ -21,7 +20,7 @@ https://api.nasa.gov/planetary/apod?api_key=wd5U8KXHEwH3VFh4LFzXqeqMIW4vUjMqn7Et
 
     
 <!-- Compnent Syntax & Naming conventions: -->
-    - id & classNames {e.g. "heading-container"}, {e.g. "heading-paragraph-btn"}
+    - id & classNames {e.g. "heading-parent-container"}, {e.g. "heading-paragraph-btn"}
 
 
 <!-- Stylesheets: -->
@@ -36,12 +35,6 @@ https://api.nasa.gov/planetary/apod?api_key=wd5U8KXHEwH3VFh4LFzXqeqMIW4vUjMqn7Et
 
     TroubleShooting Bugs:
 
-    **NOTE: if query a date, you don't receive a JSON Array, but rather a string
-    **NOTE: count=integer and start_date to end_date return JSON Arrays
-
-    a) Currently, once a specific card's like button is clicked for the first time, state is maintained upon refresh
-    b) if user makes a new query, the first card to return will have it's like button showing as active; however, if refreshed on this same card, active state is removed.  IF user return's to original card, the state is also gone.  YET a refresh on the original card will return the active state on the like button
-
-    c) If previously liked a postm you can query anything EXCEPT the original post by it's {date=} query, and the "liked" state will return false.  If you refresh while the liked posts are in view (meaning liked state is true), the state reflects this change.
+    
 
 
--- src/components/App.js
+++ src/components/App.js
@@ -14,82 +14,14 @@ window.fetch = progressBarFetch;
 
 function App() {
 
-  const [query, setQuery] = useState('');
-  const [astronomy, setAstronomy] = useState(() => {
-    const saved = localStorage.getItem('initial-posts');
-    const initialValue = JSON.parse(saved);
-    return initialValue || []
-    
-  });
- 
-  const [error, setError] = useState('');
   
-/* To dynamically render astronomy data: */
-  /* E.g. date=2017-07-08 or date=2014-10-01*/
-  const search = event => {
-    
-    if(event.key === "Enter") {
-      fetch(`${BASE_URL}${apiKey}&${query}`)
-        .then(result => result.json())
-        .then(result => {
-          setQuery('');
-          setAstronomy(result);
-          console.log("Here is astronomy data:", result);
-        } );
-    }
-  };
 
-/* Function to store queried data in LocalStorage: */
-  function storeCurrentData(postdata) {
-    localStorage.setItem('initial-posts', JSON.stringify(postdata) )
-  };
-
-/* Main API fetch call: */
-
-async function fetchData() {
-  fetch(`${BASE_URL}${apiKey}&start_date=2017-07-08&end_date=2017-07-20
-`)
-/* fetch(`${BASE_URL}${apiKey}&count=100`) */
-/* fetch(`${BASE_URL}${apiKey}`) */
-  .then((res) => {
-    if (res.ok) return res.json();
-    throw new Error('An Error occurred when fetching posts');
-  })
-  .then((posts) => setAstronomy(posts))
-  .catch((error) => setError(error.message));
-}
-
-
-/* Runs only if no data is in local Storage: */
-useEffect(() => {
- 
-  if (astronomy.length === 0) {
-    fetchData();
-  }
-    
-} , [astronomy]);
-
-
-/* Persisting the initial fetch in local-storage: */
-useEffect( () => {
-  storeCurrentData(astronomy)
-  /* console.log("here is astronomy from the useEffect: ", astronomy); */
-  /* console.log(astronomy[1])  *//* Data in astronomy is currently indexed by the return from API */
-}, [astronomy]);
-
-/* Error Handling: */
-if (error) return <h1>{error}</h1>
 
   
   return (
     <div className='App'>
 
-      <ProgressBar style={ {backgroundColor:'#36BF9D', height: '8px', borderRadius: '5px'}}/>
-      <HomePage query={query} setQuery={setQuery} astronomy={astronomy} setAstronomy={setAstronomy}
-        apiKey={apiKey}
-        BASE_URL={BASE_URL}
-        search={search}
-      />
+      
 
     </div>
   );
