d90fd6b264ed495d294643c74e77a6141fc10f8c
Trinidad Padilla <tpadilla101@live.com>
2022-05-10 17:53:09 -0700

    got the form fetching properly

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react';
 /* import axios from 'axios'; */
 import { CtaButton, ApiResponses } from '../utils';
-const { Configuration, OpenAIApi } = require("openai");
+/* const { Configuration, OpenAIApi } = require("openai"); */
 
 require('dotenv').config();
 
@@ -10,9 +10,9 @@ require('dotenv').config();
     /* const BASE_URL = 'https://api.openai.com/v1/engines/text-curie-001/completions';
     const TEST_BASE_URL = `${apiKey}https://api.openai.com/v1/engines/text-curie-001/`; */
 
-    const configuration = new Configuration({
+    /* const configuration = new Configuration({
         apiKey: apiKey,
-    });
+    }); */
 
     /* const openai = new OpenAIApi(configuration); */
 
@@ -20,26 +20,13 @@ require('dotenv').config();
 function Form( { content } ) {
 
     const formInputRef = useRef(null);
-    const [ postData, setPostData ] = useState([]);
+    const [ postData, setPostData ] = useState('');
     const [ testDataState, setTestDataState] = useState('');
-console.log(postData)
 
-/* Function to store queried data in LocalStorage: */
-    const storeApiData = (param) => {
-
-        if(postData === undefined || postData === null) {
-
-            setPostData(param);
-        } else {
-
-            postData.unshift(param)
-        }
-        localStorage.setItem('initial-responses', JSON.stringify( param ));
-    };
-
+/* The Request body for my POST Request: */
 
     const testData = {
-        prompt: `${testDataState}`,
+        prompt: postData,
         temperature: 0.5,
         max_tokens: 64,
         top_p: 1.0,
@@ -47,22 +34,16 @@ console.log(postData)
         presence_penalty: 0.0,
     };       
 
+/* The submitForm function is a Click Handler for when a user submits a form: */
+
     const submitForm = (event) => {
         event.preventDefault();
-        console.log('clicked the form button!')
-
-        try {
-            setTestDataState(formInputRef.current.value);
-        } catch(error) {
-            console.log(error);
-        }
-
+        /* console.log('The result of my useRef: ', formInputRef.current.value) */
+        console.log('Here is testData: ', testData)
         postRequest();
-        console.log('The result of my useRef: ', formInputRef.current.value)
-        console.log('The type of data from the useRef: ', typeof formInputRef.current.value)
-        /* getRequestTest(); */
-
+        setPostData('');
     };
+    
 
     /* const getRequestTest = async () => {
         try {
@@ -88,7 +69,7 @@ console.log(postData)
 
 /* The logic for a POST Request using fetch:  */
 
-    const postRequest = async => {
+    const postRequest = async () => {
         fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
             method: "POST",
             headers: {
@@ -99,12 +80,11 @@ console.log(postData)
             
         })
         .then(result => result.json() )
-            .then( result => {
-                console.log('The result of Post req with Fetch: ', result);
-                storeApiData(result);
-            })
-        ;
-
+        .then( result => {
+            console.log('The result of Post req with Fetch: ', result);
+            /* setPostData(result); */
+            localStorage.setItem('initial-responses', JSON.stringify( result ));
+        });
     };
 
     
@@ -121,7 +101,9 @@ console.log(postData)
         <form className='form'>
             <div className='form-input-wrapper'>
                 <label />
-                <textarea rows='5' placeholder='Message' name='message' required ref={formInputRef} />
+                <textarea rows='5' placeholder='Message' name='message' required ref={formInputRef} value={postData}
+                    onChange={event => setPostData(event.target.value)}
+                />
             </div>
 
     {/* Where the 'Submit' button is rendered: */}
